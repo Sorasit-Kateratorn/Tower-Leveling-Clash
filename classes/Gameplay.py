417144151d4720d0a7b910117ab053c12f3d9c2f
Sorasit Kateratorn <sorasit.ka@ku.th>
2025-03-18 00:45:42 +0700

    Config use items in gameplay

--- classes/Gameplay.py
+++ classes/Gameplay.py
@@ -1,6 +1,7 @@
 from classes.Player import *
 from classes.Enemy import *
 from classes.Save import *
+from classes.Inventory import *
 import random
 
 # Define the number of floors in the game
@@ -11,6 +12,7 @@ class GamePlay:
     def __init__(self):
         # Choose a hero
         self.player = self.choose_hero()
+        self.inventory = Inventory()
         self.current_floor = 1
 
     def choose_hero(self):
@@ -43,12 +45,19 @@ class GamePlay:
                 print("Choose an action:")
                 print("1. Attack")
                 print("2. Use Special Ability")
+                print("3. Use items")
+                print("4. Discard items")
                 choice = input("Enter choice: ")
 
                 if choice == "1":
                     self.player.attack_enemy(enemy)
                 elif choice == "2":
                     self.player.use_ability(enemy)
+                elif choice == "3":
+                    self.use_item()
+
+                # TODO discard item
+                # Show Shop for buy items
 
                 if enemy.health > 0:  # Enemy still alive, it attacks back
                     enemy.attack_enemy(self.player)
@@ -62,10 +71,24 @@ class GamePlay:
                 f"\nFloor {self.current_floor} cleared! Health remaining: {self.player.health}")
             self.current_floor += 1
 
+    def use_item(self):
+        if not self.inventory.items:
+            print("No items in inventory!")
+            return
+
+        print("\nChoose an item to use:")
+        for i, item in enumerate(self.inventory.items):
+            print(f"{i+1}. {item.name} - {item.ability}")
+
+        choice = input("Enter the item number: ")
+        if choice.isdigit() and 1 <= int(choice) <= len(self.inventory.items):
+            # Pass player to apply effect
+            self.inventory.use_item(int(choice) - 1, self.player)
+
     def check_game_over(self):
         if self.player.health <= 0:
             print("\nGame Over!")
-            # save_data([self.current_floor, "Lost"])
+            # save_data([self.current_floor, "Lost")
         else:
             print("\nVictory! You cleared all floors!")
-            # save_data([FLOORS, "Won"])
+            # save_data(["Won")
